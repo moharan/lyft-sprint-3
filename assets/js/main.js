c918260f9cda35737a2640630e25b1be2accb24c
RangiBecerra <rangibecerra@gmail.com>
2017-05-23 13:04:09 -0400

    addEvenListener desde JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 //alert("Proyecto final");
+var lista = function() {
+    alert("existe");
+}
+
+function principal() {
+    var funcion_01 = document.getElementById("become");
+    funcion_01.addEventListener("click", lista);
+};
+
+principal();
