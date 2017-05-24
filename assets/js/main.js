6aa3dde3119b3b83b22091ab36a99696c756984a
RangiBecerra <rangibecerra@gmail.com>
2017-05-24 02:10:03 -0400

    validacion input por vacio y numeros

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,60 @@
 //alert("Proyecto final");
 var digitos = /^[0-9]$/;
+var letras = /^[A-Za-z]*$/;
 
 var number = function() {
     var numero = document.getElementById("phonenumber").value;
-    //var num = parseInt(numero); 
-    console.log(numero);
+    //var num = parseInt(numero);
+    //isNaN(num);
+    //console.log(isNaN(num));
 // num === null || num.length === 0 || num != "NaN"
-    if (numero === null || numero.length === 0) {
+    if (numero == null || numero.length == 0 || numero =="") {
     	alert("Esta vacio");
         console.log(numero + "Esta vacio");
-    } else {
-    	alert("Es Valido");
+    } else if (isNaN(parseInt(numero))) {
+    	alert("Debe ingresar números");
+        console.log(numero + "Debe ingresar números");
+    }/*else if (!digitos.test(num)) {
+    	alert("Debe ser digitos");
+    	console.log(num + "Debe ser digitos");
+    } else if (isNaN(num)) {
+    	alert("NaN?");
+    	console.log(num + "NaN?");
+    }*/ else {
+    	alert("Ingreso Datos");
+    	console.log(numero + "Ingreso Datos");
     }
+
+    document.getElementById("phonenumber").value = "";
 }
 
 var juego = function() {
 	// value de los input ingresados para jugar
     var auto01 = document.getElementById("auto01").value;
     console.log(auto01);
+
+    if (auto01 == null || auto01.length == 0 || auto01 =="") {
+    	alert("Debe ingresar Numeros entre el 1 y 10");
+    } else if (isNaN(parseInt(auto01))) {
+    	alert("Debe ingresar números");
+        console.log(auto01 + "Debe ingresar números para X");
+    } else {
+    	alert("Ingreso Datos para X");
+    	console.log(auto01 + "Ingreso Datos para X");
+    }
+
     var auto02 = document.getElementById("auto02").value;
     console.log(auto02);
+
+    if (auto02 == null || auto02.length == 0 || auto02 =="") {
+    	alert("Debe ingresar Numeros entre el 1 y 6");
+    } else if (isNaN(parseInt(auto02))) {
+    	alert("Debe ingresar números");
+        console.log(auto02 + "Debe ingresar números para Y");
+    } else {
+    	alert("Ingreso Datos para Y");
+    	console.log(auto02 + "Ingreso Datos para Y");
+    }
     // creando tablero desde js
   	// visible o invisible start (que son las indicaciones de tablero)
   	var autitos = document.getElementById("autitos");
@@ -39,7 +74,8 @@ var juego = function() {
 		  [0,0,0,0,0,0,0,0,0,0]
 		]
 		//Ingreso el auto en la posicion 3,5
-		tablero[3][5] = "A";
+		// esta ingresando valores del value auto01 - auto02 *NO FUNCIONA 100%*
+		tablero[auto01][auto02] = "x";
 
 		//Obtengo el div en donde se dibujará el tablero
 		var divTablero = document.getElementById("tablero");
